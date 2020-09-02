548a6853851199420e93e2b485548547aa632d2f
Vladislav_747 <zippo585@gmail.com>
2020-09-02 18:00:09 +0500

    Added eventListeners for Banners, made minimizeText, Almost finish css

--- README.md
+++ README.md
@@ -5,3 +5,12 @@
 
  https://www.figma.com/file/yXeGSF4f0UTciZWfIYKdx9/MST-%7C-Frontend-Test---1?node-id=9%3A208
 
+ # Stack
+ - React
+ - SCSS
+
+ Проблема возникла со следующим функционалом http://joxi.ru/GrqWENlUko9NY2 
+
+
+
+Изначально хотел сделать подчеркивание так http://joxi.ru/82QbgzjcyYkedA но не все браузеры это поддерживают
--- js/script.js
+++ js/script.js
@@ -18,6 +18,46 @@ $(document).ready(function() {
         
     );
 
+
+         //Обрезать текст
+        function minimizeText(lengthStr){
+            
+            console.log($('.banner-section__subtitle-text span').text().length, "Длина"); 
+            if($('.banner-section__subtitle-text span').text().length < lengthStr){
+                console.log("Не режем"); 
+
+            }else{
+                console.log("Режем"); 
+                console.log($('.banner-section__subtitle-text span').parent(), "Родительский элемент");
+                var originalText = $('.banner-section__subtitle-text span').text();
+                var newText = originalText.substr(0, lengthStr);
+                $('.banner-section__subtitle-text span').text(newText);
+                let linkDetailEl = "<span class='link-detail-element'><svg width='28' height='28' viewBox='0 0 28 28' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M0 0H28V28H0V0Z' fill='#262525'/><path fill-rule='evenodd' clip-rule='evenodd' d='M8.5 13H10.7395V15.2406H8.5V13ZM12.9803 13H15.2197V15.2406H12.9803V13ZM19.7001 13H17.4592V15.2406H19.7001V13Z' fill='white'/></svg></span>";
+                console.log($('.banner-section__subtitle-text span').parent().append(linkDetailEl), "Родительский элемент"); 
+            }
+
+            if($('.banner-section__subtitle-text').hasClass('hidden')){
+                $('.banner-section__subtitle-text').removeClass('hidden');
+            }
+        }
+
+
+   minimizeText(100);
+
+
+   $(".left-menu__sections ul li").click(function(){
+        console.log("clickBanners");
+        console.log($(this), "element");
+        var bannerLinks = $('.left-menu__sections ul li');
+        console.log( bannerLinks);
+        for(var i = 0; i < bannerLinks.length; i++){
+            bannerLinks[i].classList.remove("active");
+        }
+        $(this).addClass("active");
+        console.log( $('.left-menu__sections ul li'));
+   });
+
+
   });
 
   $(window).resize(function(){
